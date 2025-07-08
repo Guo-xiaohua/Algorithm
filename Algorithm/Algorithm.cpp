20a7850850ffa0cec0e3b0d17c9682f3fc7d513d
郭小华 <2064214019@qq.com>
2025-07-08 22:02:14 +0800

    堆排序

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -3,43 +3,62 @@
 #include <vector>
 using namespace std;
 
-void ShellSort(vector<int>& vec,int n)
+
+/// 
+/// @param vec 要维护的数组
+/// @param n 数组大小 // 排序使用 
+/// @param i 要维护的父节点
+void Heapify(vector<int>& vec,int n,int i)
 {
-    int gap = n;
+    int parent = i;
+    int lson = i*2+1;
+    int rson = i*2+2;
 
-    while (gap>1)
+    if (lson<n && vec[parent]<vec[lson])
+        parent=lson;
+    if (rson<n && vec[parent]<vec[rson])
+        parent=rson;
+    if (parent!=i)
     {
-        gap = gap/2;
-
-        for (int i=gap;i<n;i++)
-        {
-            if (vec[i] < vec[i - gap])
-            {
-                int tmp = vec[i];
-                int j;
-                for (j = i - gap; j >= 0 && vec[j] > tmp; j-=gap)//这里是j-=gap
-                {
-                    vec[j + gap] = vec[j];
-                }
-                vec[j + gap] = tmp;
-            }
-        }
+        swap(vec[i],vec[parent]);
+        Heapify(vec,n-1,parent);
     }
     
 }
 
+void HeapSort(vector<int>& vec, int n)
+{
+    // 建堆 先从数组的最后一个节点开始
+    // 下标为i的节点,他的父节点下标为(i-1)/2  // 整数取整
+    // 下标为0的点最后一个维护能保证堆顶是最大的,然后递归对其孩子节点进行维护保证大顶堆
+    for (int i=n/2-1; i>=0; i--)
+        Heapify(vec,n,i);
+    
+    // 排序
+    for (int i=n-1; i>0; i--)
+    {
+        swap(vec[i],vec[0]);// 将最大的堆顶移到末尾,对剩下的维护大顶堆,保证剩下元素最大的为堆顶
+        Heapify(vec,i,0);
+    }
+
+    
+}
+
 /*
- * 快速排序    递归实现
- * 两个指针,一个指头,一个指尾
- * 默认最左边为待比较对象,一次排序实现左边全比待比较对象小,右边全比待比较对象大
- * 然后对左右两边分别念进行快速排序
+ * 堆排序
+ *
+ * 父节点下标为 i
+ * 左孩子下标 i*2+1
+ * 右孩子下标 i*2+2
+ *
+ * 升序采用大顶堆,及任意一个父节点满足父节点大于连个孩子节点
  */
 
 int main()
 {
     vector<int> vec = {9,8,7,6,5,4,3,2,1};
 
-    ShellSort(vec,vec.size());
+    HeapSort(vec,vec.size());
     
     for (int i=0;i<vec.size();i++)
     {
