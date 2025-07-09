d8120e9a704f17cd6622edb662b32735bb40c4db
郭小华 <2064214019@qq.com>
2025-07-09 16:41:11 +0800

    图的拓扑排序

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,69 +1,68 @@
 ﻿#include <iostream>
-#include <stack>
 #include <vector>
+#include <queue>
 using namespace std;
 
 
-/// 
-/// @param vec 要维护的数组
-/// @param n 数组大小 // 排序使用 
-/// @param i 要维护的父节点
-void Heapify(vector<int>& vec,int n,int i)
-{
-    int parent = i;
-    int lson = i*2+1;
-    int rson = i*2+2;
 
-    if (lson<n && vec[parent]<vec[lson])
-        parent=lson;
-    if (rson<n && vec[parent]<vec[rson])
-        parent=rson;
-    if (parent!=i)
-    {
-        swap(vec[i],vec[parent]);
-        Heapify(vec,n-1,parent);
-    }
-    
-}
-
-void HeapSort(vector<int>& vec, int n)
-{
-    // 建堆 先从数组的最后一个节点开始
-    // 下标为i的节点,他的父节点下标为(i-1)/2  // 整数取整
-    // 下标为0的点最后一个维护能保证堆顶是最大的,然后递归对其孩子节点进行维护保证大顶堆
-    for (int i=n/2-1; i>=0; i--)
-        Heapify(vec,n,i);
-    
-    // 排序
-    for (int i=n-1; i>0; i--)
-    {
-        swap(vec[i],vec[0]);// 将最大的堆顶移到末尾,对剩下的维护大顶堆,保证剩下元素最大的为堆顶
-        Heapify(vec,i,0);
-    }
-
-    
-}
-
-/*
- * 堆排序
- *
- * 父节点下标为 i
- * 左孩子下标 i*2+1
- * 右孩子下标 i*2+2
- *
- * 升序采用大顶堆,及任意一个父节点满足父节点大于连个孩子节点
- */
-
+int n, m, x, y;
 int main()
 {
-    vector<int> vec = {9,8,7,6,5,4,3,2,1};
-
-    HeapSort(vec,vec.size());
-    
-    for (int i=0;i<vec.size();i++)
+    cin>>n>>m;
+    // 记录所有节点的入度
+    vector<int> inds(n + 1, 0);
+    // 图
+    vector<vector<int>> tu(n+1);
+    // 图的读取
+    while (m--)
     {
-        cout<<vec[i]<<" ";
+        cin>>x>>y;
+        // 记录节点的入度
+        inds[y]++;
+        tu[x].push_back(y);
     }
-    return 0;    
-}
 
+    // 排序用的辅助队列,仅将入度为0的点放入
+    queue<int> que;
+
+    // 遍历图,将入度为0的点入队
+    for (int i=1;i<=n;i++)
+    {
+        if (inds[i] == 0)
+            que.push(i);
+    }
+
+    // 处理队列,将入度为0的点出队,并将其指向的点入度-1,
+    // 若该点入度为0,则入队
+    vector<int> ans;
+    while (!que.empty())
+    {
+        int i = que.front();
+        que.pop();
+        ans.push_back(i);
+        for (int j=0;j<tu[i].size();j++)
+        {
+            if (tu[i][j]>0)
+            {
+                inds[tu[i][j]]--;
+                if (inds[tu[i][j]] == 0)
+                    que.push(tu[i][j]);
+            }
+        }
+    }
+
+    // 输出
+    if (ans.size()!=n)
+        cout<<-1<<endl;
+    else
+    {
+        for (int i=0;i<ans.size();i++)
+        {
+            cout<<ans[i];
+            if (i+1<ans.size())
+                cout<<" ";
+        }
+    }
+    
+    return 0;
+}
