a2afca500a0a754c66d03cb1b2058c98d2cc670a
郭小华 <2064214019@qq.com>
2025-07-29 23:06:21 +0800

    bfs求解迷宫问题

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -1,68 +1,83 @@
 ﻿#include <iostream>
+#include <stack>
 #include <vector>
-#include <queue>
-using namespace std;
 
-
-
-int n, m, x, y;
-int main()
+struct Position
 {
-    cin>>n>>m;
-    // 记录所有节点的入度
-    vector<int> inds(n + 1, 0);
-    // 图
-    vector<vector<int>> tu(n+1);
-    // 图的读取
-    while (m--)
-    {
-        cin>>x>>y;
-        // 记录节点的入度
-        inds[y]++;
-        tu[x].push_back(y);
-    }
+    int x,y;
+    int Size;
+    
+    Position(int a=0,int b=0,int size=0):x(a),y(b),Size(size){}
+};
 
-    // 排序用的辅助队列,仅将入度为0的点放入
-    queue<int> que;
-
-    // 遍历图,将入度为0的点入队
-    for (int i=1;i<=n;i++)
+int main() 
+{
+    int n,m;
+    std::cin>>n>>m;
+    // 输入起点与终点坐标
+    int Xs,Ys,Xe,Ye;
+    std::cin>>Xs>>Ys>>Xe>>Ye;
+    
+    std::vector<std::vector<char>> arr(n+1,std::vector<char>(m+1,' '));
+    for(int i=1;i<=n;i++)
     {
-        if (inds[i] == 0)
-            que.push(i);
-    }
-
-    // 处理队列,将入度为0的点出队,并将其指向的点入度-1,
-    // 若该点入度为0,则入队
-    vector<int> ans;
-    while (!que.empty())
-    {
-        int i = que.front();
-        que.pop();
-        ans.push_back(i);
-        for (int j=0;j<tu[i].size();j++)
+        for(int j=1;j<=m;j++)
         {
-            if (tu[i][j]>0)
+            std::cin>>arr[i][j];
+        }
+    }
+
+    if (arr[Xe][Ye] == '*')
+    {
+        std::cout<<"-1"<<std::endl;
+        return 0;
+    }
+    if (Xs == Xe && Ys == Ye)
+    {
+        std::cout<<"0"<<std::endl;
+        return 0;
+    }
+
+    std::vector<std::vector<bool>> visited(n+1,std::vector<bool>(m+1,false));
+    std::stack<Position> Stack;
+    Stack.push(Position(Xs,Ys,0));
+    int MinSize = INT_MAX;
+
+    int NewX[4]{1,-1,0,0};
+    int NewY[4]{0,0,-1,1};
+    
+    while (!Stack.empty())
+    {
+        Position p = Stack.top();
+        Stack.pop();
+
+        if (p.x == Xe && p.y == Ye)
+        {
+            MinSize = std::min(MinSize,p.Size);
+            break;
+        }
+        
+        // 检查4个方向能不能走
+        for (int i=0;i<4;i++)
+        {
+            int nx = p.x + NewX[i];
+            int ny = p.y + NewY[i];
+
+            // 边界检查
+            if (nx<1 || nx>n || ny<1 || ny>m)
+                continue;
+
+            if (arr[nx][ny] != '*' && arr[nx][ny] != ' ' && !visited[nx][ny])
             {
-                inds[tu[i][j]]--;
-                if (inds[tu[i][j]] == 0)
-                    que.push(tu[i][j]);
+                Stack.push(Position(nx,ny,p.Size+1));
+                visited[nx][ny] = true;
             }
         }
     }
-
-    // 输出
-    if (ans.size()!=n)
-        cout<<-1<<endl;
+    if (MinSize!=INT_MAX)
+        std::cout<<MinSize<<std::endl;
     else
-    {
-        for (int i=0;i<ans.size();i++)
-        {
-            cout<<ans[i];
-            if (i+1<ans.size())
-                cout<<" ";
-        }
-    }
-    
+        std::cout<<"-1"<<std::endl;
+
     return 0;
 }
